8315d9377997188ef4694f1c6142f2b54c783dff
aviatesk <aviatesk@gmail.com>
2019-10-21 02:22:12 +0900

    2019-G: theequations: up to visible testset

--- 2019-G/bookreading.jl
+++ 2019-G/bookreading.jl
@@ -1,51 +1,12 @@
-#=
-
-### Limits
-
-Time limit: 40 seconds per test set.
-Memory limit: 1GB.
-1 ≤ T ≤ 100.
-1 ≤ P₁ < P₂ < ... < P_M ≤ N.
-1 ≤ R_i ≤ N, for all i.
-
-Test set 1 (Visible)
-1 ≤ M ≤ N ≤ 1000.
-1 ≤ Q ≤ 1000.
-
-Test set 2 (Hidden)
-1 ≤ M ≤ N ≤ 10⁵.
-1 ≤ Q ≤ 10⁵.
-
-### Input
-
-T
-N M Q
-P... (M elements)
-R... (Q elements)
-...
-
-### Sample case
-
-3
-11 1 2
-8
-2 3
-11 11 11
-1 2 3 4 5 6 7 8 9 10 11
-1 2 3 4 5 6 7 8 9 10 11
-1000 6 1
-4 8 15 16 23 42
-1
-
-=#
-
-# --- utils
+# utils
+# ----
 
 readnum(type::Type{<:Number} = Int) = parse(type, readline())
 readarray(type::Type{<:Number} = Int, dlm = isspace; kwargs...) =
     parse.(type, split(readline(), dlm; kwargs...))
 
-# --- body
+# body
+# ----
 
 function main()
     T = readnum()
@@ -67,6 +28,7 @@ function solve(N, P, R)
     sum(ps[r] for r in R) # `pages` should be pre-computed, since R can be [1, 1, 1, ...]
 end
 
-# --- call
+# call
+# ----
 
 main()
--- 2019-G/theequation.jl
+++ 2019-G/theequation.jl
@@ -1,53 +1,20 @@
-#=
+# TODO: hidden testset
 
-### Limits
-
-Time limit: 15 seconds per test set.
-Memory limit: 1GB.
-1 ≤ T ≤ 100.
-1 ≤ N ≤ 1000.
-
-Test set 1 (Visible)
-0 ≤ M ≤ 100.
-0 ≤ A_i ≤ 100, for all i.
-
-Test set 2 (Hidden)
-0 ≤ M ≤ 10^{15}.
-0 ≤ A_i ≤ 10^{15}, for all i.
-
-
-### Input
-
-T
-N M
-A... (N elements)
-...
-
-### Sample case
-
-4
-3 27
-8 2 4
-4 45
-30 0 4 11
-1 0
-100
-6 2
-5 5 1 5 1 0
-
-=#
-
-# ---
+# utils
+# ----
 
 readnum(type::Type{<:Number} = Int) = parse(type, readline())
 readarray(type::Type{<:Number} = Int, dlm = isspace; kwargs...) =
     parse.(type, split(readline(), dlm; kwargs...))
 
+# body
+# ----
+
 function main()
     T = readnum()
     for t = 1:T
         N, M = readarray()
-        A = readarray()
+        A = readarray() # N elements
         k = solve(N, M, A)
         println(stdout, "Case #$(t): $(k)")
     end
@@ -55,13 +22,15 @@ end
 
 function solve(N, M, A)
     ret = -1
-    for k = 1:1000 # TODO: for hidden testset
-        s = sum(k ⊻ Ai for Ai in A)
-        s <= M && (ret = k)
+    for k = 0:127
+        if sum(Ai ⊻ k for Ai in A) ≤ M
+            ret = k
+        end
     end
     ret
 end
 
-# ---
+# call
+# ----
 
 main()
