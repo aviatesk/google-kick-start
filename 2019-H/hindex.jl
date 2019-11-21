a6913a6a9ea4f763c3c0f29df331dddd7e0be3d9
aviatesk <aviatesk@gmail.com>
2019-11-21 11:43:51 +0900

    2019-H: finish up H-index

--- 2019-H/hindex.jl
+++ 2019-H/hindex.jl
@@ -1,40 +1,58 @@
 # utils
 # ----
 
-readnum(type::Type{<:Number} = Int) = parse(type, readline())
-readarray(type::Type{<:Number} = Int, dlm = isspace; kwargs...) =
-    parse.(type, split(readline(), dlm; kwargs...))
+readnum(io = stdin, T::Type{<:Number} = Int) = parse(T, readline(io))
+readarray(io = stdin, T::Type{<:Number} = Int, dlm = isspace; kwargs...) =
+    parse.(T, split(readline(io), dlm; kwargs...))
 
 # body
 # ----
 
-function main()
-    T = readnum()
+function main(io = stdin)
+    T = readnum(io)
     for t = 1:T
-        N = readnum()
-        A = readarray() # N elements
+        N = readnum(io)
+        A = readarray(io) # N elements
         hs = solve(N, A)
         println(stdout, "Case #$(t): $(join(hs, ' '))")
     end
 end
 
 function solve(N, A)
-    [hindex(i, A[1:i]) for i = 1:N]
-end
+    ret = Vector{Int}(undef, N)
+    memo = Dict{Int, Int}()
+    ans = 0
+    rm = 1
+    size = 0
 
-function hindex(n, A′)
-    sort!(A′; rev = true)
-
-    maxw = 0
-    for (w, h) in enumerate(A′)
-        if w ≤ h
-            maxw = w
+    for (i, a) in enumerate(A)
+        if a > ans
+            memo[a] = get!(memo, a, 0) + 1
+            size += 1
         end
+
+        for k in rm:ans
+            size -= get!(memo, k, 0)
+        end
+        rm = ans + 1
+
+        if size ≥ ans + 1
+            ans += 1
+        end
+
+        ret[i] = ans
     end
-    maxw
+
+    ret
 end
 
 # call
 # ----
 
-main()
+if isdefined(Main, :Juno)
+    open("2019-H/_hindex.txt") do f
+        main(f)
+    end
+else
+    main()
+end
